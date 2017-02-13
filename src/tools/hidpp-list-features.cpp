6b03261d902b67e90aa6dbf8ccf8daf5b77e767a
Clément Vuchener <clement.vuchener@gmail.com>
2017-02-13 19:22:24 +0100

    hidpp-list-features: add known HID++2.0 feature names

--- src/tools/hidpp-list-features.cpp
+++ src/tools/hidpp-list-features.cpp
@@ -17,6 +17,7 @@
  */
 
 #include <cstdio>
+#include <map>
 
 #include <hidpp/Device.h>
 #include <hidpp10/Device.h>
@@ -29,6 +30,67 @@
 #include "common/common.h"
 #include "common/Option.h"
 #include "common/CommonOptions.h"
+
+static const std::map<uint16_t, const char *> HIDPP20Features = {
+	{ 0x0000, "Root" },
+	{ 0x0001, "Feature set" },
+	{ 0x0002, "Feature info" },
+	{ 0x0003, "Device FW version" },
+	{ 0x0005, "Device name" },
+	{ 0x0006, "Device groups" },
+	{ 0x0020, "Reset" },
+	{ 0x00c0, "DFUcontrol" },
+	{ 0x00c1, "DFUcontrol 2" },
+	{ 0x00d0, "DFU" },
+	{ 0x1000, "Battery status" },
+	{ 0x1300, "LED control" },
+	{ 0x1814, "Change host" },
+	{ 0x1981, "Backlight" },
+	{ 0x1b00, "Reprog controls" },
+	{ 0x1b01, "Reprog controls v2" },
+	{ 0x1b02, "Reprog controls v2.2" },
+	{ 0x1b03, "Reprog controls v3" },
+	{ 0x1b04, "Reprog controls v4" },
+	{ 0x1d4b, "Wireless device status" },
+	{ 0x2001, "Left right swap" },
+	{ 0x2005, "Swap button" },
+	{ 0x2100, "Vertical scrolling" },
+	{ 0x2110, "Smart shift" },
+	{ 0x2120, "Hi-res scrolling" },
+	{ 0x2121, "Hi-res wheel" },
+	{ 0x2130, "Low-res wheel" },
+	{ 0x2200, "Mouse pointer" },
+	{ 0x2201, "Adjustable dpi" },
+	{ 0x2205, "Pointer speed" },
+	{ 0x2230, "Angle snapping" },
+	{ 0x2240, "Surface tuning" },
+	{ 0x2400, "Hybrid tracking" },
+	{ 0x40a0, "Fn inversion" },
+	{ 0x40a2, "New fn inversion" },
+	{ 0x4100, "Encryption" },
+	{ 0x4220, "Lock key state" },
+	{ 0x4301, "Solar dashboard" },
+	{ 0x4520, "Keyboard layout" },
+	{ 0x4521, "Keyboard disable" },
+	{ 0x4530, "Dualplatform" },
+	{ 0x4540, "Keyboard layout 2" },
+	{ 0x6010, "Touchpad FW items" },
+	{ 0x6011, "Touchpad SW items" },
+	{ 0x6012, "Touchpad Win8 FW items" },
+	{ 0x6100, "Touchpad raw xy" },
+	{ 0x6110, "Touchmouse raw points" },
+	{ 0x6120, "Touchmouse 6120" },
+	{ 0x6500, "Gesture" },
+	{ 0x6501, "Gesture 2" },
+	{ 0x8010, "G-key" },
+	{ 0x8020, "M-keys" },
+	{ 0x8030, "MR" },
+	{ 0x8060, "Report rate" },
+	{ 0x8070, "Color LED effects" },
+	{ 0x8080, "Per-key lighting" },
+	{ 0x8100, "Onboard profiles" },
+	{ 0x8110, "Mouse button spy" },
+};
 
 void testRegister (HIDPP10::Device *dev, std::size_t register_size, uint8_t address, bool test_write = false)
 {
@@ -194,13 +256,29 @@ int main (int argc, char *argv[])
 			bool obsolete, hidden, internal;
 
 			feature_id = ifeatureset.getFeatureID (feature_index, &obsolete, &hidden, &internal);
-			printf ("Feature 0x%02hhx: [0x%04hx]", feature_index, feature_id);
+			auto str = HIDPP20Features.find (feature_id);
+			printf ("Feature 0x%02hhx: [0x%04hx] %s",
+				feature_index, feature_id,
+				(str == HIDPP20Features.end () ? "?" : str->second));
+			std::vector<const char *> flag_strings;
 			if (obsolete)
-				printf (" obsolete");
+				flag_strings.push_back ("obsolete");
 			if (hidden)
-				printf (" hidden");
+				flag_strings.push_back ("hidden");
 			if (internal)
-				printf (" internal");
+				flag_strings.push_back ("internal");
+			if (!flag_strings.empty ()) {
+				printf (" (");
+				bool first = true;
+				for (auto str: flag_strings) {
+					if (first)
+						first = false;
+					else
+						printf (", ");
+					printf ("%s", str);
+				}
+				printf (")");
+			}
 			printf ("\n");
 		}
 	}
